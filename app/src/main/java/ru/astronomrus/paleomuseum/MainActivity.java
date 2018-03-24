f883198faea6b11e09ebf063ed892a72e513872b
wkoroy <wkoroy@gmail.com>
2018-03-24 21:17:06 +0300

    добавил переход  по  геохронологической шкале!

--- app/src/main/java/ru/astronomrus/paleomuseum/MainActivity.java
+++ app/src/main/java/ru/astronomrus/paleomuseum/MainActivity.java
@@ -1,5 +1,6 @@
 package ru.astronomrus.paleomuseum;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
@@ -12,6 +13,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -120,6 +128,11 @@ public static  DrawerLayout mdrawer;
             fragment = new WebViewFragment();
             fragment.setArguments(bundle);
         }
+        else if (id == R.id.photo_by_geochrones) {
+
+            show_list_geochrones();
+
+        }
         if (fragment != null) {
             FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
             ft.replace(R.id.content_frame, fragment);
@@ -130,6 +143,62 @@ public static  DrawerLayout mdrawer;
         return true;
     }
 
+  void show_list_geochrones()
+  {
+      int req_pg  = 1;
+      final Dialog dialog = new Dialog(MainActivity.ctx);
+      dialog.setContentView(R.layout.dlg_list_geochrones);
+      dialog.setTitle("Век / Ярус (Stage)");
+      dialog.show();
+      final TextView title = (TextView) dialog.findViewById(R.id.lgh_title);
+      final GridView  listView  = (GridView) dialog.findViewById(R.id.lgh_grid_geochrones);
 
+      String []geochrones = new String[tag_geochrones.length];
+      for(int i=0;i<tag_geochrones.length;i++)
+      {
+          geochrones[i] = new String(tag_geochrones[i].name);
+      }
+      ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+              android.R.layout.simple_list_item_1, android.R.id.text1, geochrones );
+
+      // Assign adapter to ListView
+      listView.setAdapter(adapter);
+
+      listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+          @Override
+          public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+              Fragment fragment = null;
+              Bundle bundle = new Bundle();
+              bundle.putString(GalleryFragment.BKEY_URL, "https://ammonit.ru/geochrono/"+tag_geochrones[i].tag+"/popfotos/");
+              fragment = new GalleryFragment();
+              fragment.setArguments(bundle);
+              if (fragment != null) {
+                  FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                  ft.replace(R.id.content_frame, fragment);
+                  ft.commit();
+                  dialog.hide();
+              }
+          }
+      });
+
+      Button ok = (Button) dialog.findViewById(R.id.lgh_ok);
+      Button cancel = (Button) dialog.findViewById(R.id.lgh_cancel);
+      ok.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View view) {
+            //  pnum = seek.getProgress();
+             // (new GalleryFragment.Imgitems_getter()).execute(url+pnum);
+              dialog.hide();
+          }
+      });
+
+      cancel.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View view) {
+              dialog.hide();
+          }
+      });
+  }
 
 }
