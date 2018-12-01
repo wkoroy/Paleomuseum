b930001a8c8c4c9786cb6f42838e1d8494d19e04
wkoroy <wkoroy@gmail.com>
2018-12-01 17:54:31 +0300

    Добавлен поиск по тегу!

--- app/src/main/java/ru/astronomrus/paleomuseum/MainActivity.java
+++ app/src/main/java/ru/astronomrus/paleomuseum/MainActivity.java
@@ -2,6 +2,7 @@ package ru.astronomrus.paleomuseum;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
@@ -12,16 +13,24 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.KeyEvent;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Arrays;
+import java.util.List;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -30,6 +39,8 @@ public class MainActivity extends AppCompatActivity
     static Paleotag[] tag_geochrones;
     static Paleotag[] tag_places;
     static Paleotag[] tag_bioclass;
+
+    EditText sr_field;
     Dialog dialog ;
     GalleryFragment galleryFragment;
     @Override
@@ -128,6 +139,11 @@ public class MainActivity extends AppCompatActivity
         else if (id == R.id.photo_by_bioclass) {
 
             show_list_bioclasses();
+
+        }
+        else if (id == R.id.photo_by_tag) {
+
+            show_tag_search();
 
         }
         if (fragment != null) {
@@ -281,6 +297,70 @@ public class MainActivity extends AppCompatActivity
 
     }
 
+    void show_tag_search() {
+        int req_pg = 1;
+        dialog = new Dialog(MainActivity.ctx);
+        dialog.setContentView(R.layout.dlg_search_from_tag);
+        dialog.setTitle("");
+        dialog.show();
+         TextView title = (TextView) dialog.findViewById(R.id.ts_title);
+        title.setText("Поиск по тегу");
+
+           sr_field = (EditText) dialog.findViewById(R.id.ts_etext);
+
+        /*final GridView listView = (GridView) dialog.findViewById(R.id.lgh_grid_tags);
+
+        String[] geoplaces = new String[tag_places.length];
+        for (int i = 0; i < tag_places.length; i++) {
+            geoplaces[i] = new String(tag_places[i].name);
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, geoplaces);
+
+        // Assign adapter to ListView
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                Fragment fragment = null;
+                Bundle bundle = new Bundle();
+                bundle.putString(GalleryFragment.BKEY_URL, "https://ammonit.ru/place/" + tag_places[i].tag + "/popfotos/");
+                galleryFragment = new GalleryFragment();;
+                fragment = galleryFragment;
+                fragment.setArguments(bundle);
+                if (fragment != null) {
+                    FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                    ft.replace(R.id.content_frame, fragment);
+                    ft.commit();
+                    dialog.hide();
+                }
+            }
+        });
+*/
+        Button ok = (Button) dialog.findViewById(R.id.ts_ok);
+        Button cancel = (Button) dialog.findViewById(R.id.ts_cancel);
+        ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //
+
+                (new TagPages_getter()).execute("https://ammonit.ru/tags.htm",sr_field.getText().toString());
+
+                dialog.hide();
+
+            }
+        });
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.hide();
+            }
+        });
+    }
+
 
 
     @Override
@@ -334,6 +414,101 @@ public class MainActivity extends AppCompatActivity
 
         return pt;
     }
+
+
+    public    class TagPages_getter extends AsyncTask<String, String, String> {
+        String htmlcode = "";
+        String text="";
+        String paleotime ="";
+        String place="";
+        String paleotype="";
+        boolean is_reset= false;
+
+
+
+        @Override
+        protected String doInBackground(String... strings) {
+            //выполняем запрос и получаем ответ
+            try {
+                // is_reset = strings[0];
+                Log.d("URL", strings[0]);
+                Log.d("TAG ", strings[1]);
+                text = strings[1];
+
+
+                OkHttpClient clientd = new OkHttpClient();
+                Request requestd = new Request.Builder().url(strings[0])
+                        .build();
+                Response responsed = clientd.newCall(requestd).execute();
+                String resultd = responsed.body().string();
+                // Log.d("RESPD" , resultd);
+                htmlcode = resultd.toLowerCase();
+
+                int sindex=htmlcode.indexOf(strings[1].toLowerCase());
+
+                if(-1 == sindex) return null;
+
+                int prev_sindex = sindex - strings[1].length() - 20;
+
+                if( -1 == prev_sindex) return null;
+                String cut_htmlcode = htmlcode.substring(prev_sindex,sindex);
+
+                final String tmpl1 = "href=\"tag/";
+                String tag_number =
+                        cut_htmlcode.substring(cut_htmlcode.indexOf(tmpl1)+tmpl1.length(),
+                                cut_htmlcode.indexOf(".htm"));
+
+                Log.d("URLTAG ", tag_number);
+
+                String urltag = "https://ammonit.ru/tag/"+tag_number+"/popfotos/";
+                return urltag;
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            Toast.makeText(getBaseContext(),"Поиск по тегу...",
+                    Toast.LENGTH_SHORT).show();
+
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            if(result == null) {
+
+                 Toast t = Toast.makeText(getBaseContext(), "тег ["+text+"] не найден!", Toast.LENGTH_SHORT);
+                t.setGravity(Gravity.TOP | Gravity.CENTER_HORIZONTAL, 0, 0);
+                t.show();
+                return;
+            }
+
+            Log.d("TAGRES" , result);
+
+
+            Fragment fragment = null;
+            Bundle bundle = new Bundle();
+            bundle.putString(GalleryFragment.BKEY_URL, result);
+            galleryFragment = new GalleryFragment();;
+            fragment = galleryFragment;
+            fragment.setArguments(bundle);
+            if (fragment != null) {
+
+                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                ft.replace(R.id.content_frame, fragment);
+                ft.commit();
+
+            }
+
+
+        }
+    };
 
 
 }
